0cab6dc305854bcb431c54771c5278d9b10e452d
ArlagaddaHepseeba-git <arlagaddahepseeba100@gmail.com>
2025-09-26 12:49:48 +0530

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -6,3 +6,9 @@ cd /var/www/html
 git clone https://github.com/CleverProgrammers/pwj-netflix-clone.git
 mv pwj-netflix-clone/* .
 tail -100f /var/log/httpd/access_log
+
+
+
+789632
+geyuokj
+hepseryopkjhghjk
